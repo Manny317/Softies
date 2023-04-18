d3b270a073a8b1127dca900939e396f70933326d
Emmanuel Jimenez <emmanueljimenez@Emmanuels-MacBook-Pro.local>
2023-04-17 21:24:38 -0700

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,26 @@
+
+
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Method
+    public class viewUser() {
+
+    }
+    // Method
+    public class isConnected() {
+
+    }
+    // Method
+    public class disconnected(){
+
+    }
+
+
+
+
+
+}
